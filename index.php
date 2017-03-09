075c301127214c73da13ef5f471a813062605848
Dhiraj Basnet <dhirajbasnet@Dhirajs-MacBook-Pro.local>
2017-03-09 17:33:56 +0545

    adding index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+	<title>My First PHP Class</title>
+</head>
+
+<body>
+<?php
+	//$x= 11223;
+	//echo $x;
+
+	$x = 121.34;
+	echo $x;
+	echo '<br>';	
+	$x = (int) $x;
+	echo $x;
+?>
+</body>
+</html>
